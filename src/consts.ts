d56c045d2b920eba5444aa87a3304d255fea1445
Joseph Meunier <joseph.meunier@proton.me>
2026-06-02 00:46:26 +0200

    fix: update SITE_TITLE to reflect correct author name

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Gaston De Sade';
+export const SITE_TITLE = 'Joseph Meunier';
 export const SITE_DESCRIPTION = 'Learn & build in public';
 export const isProd = import.meta.env.MODE === 'production';
